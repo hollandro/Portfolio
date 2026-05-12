3864d17e655e383fd4fbcea24920d6304f5dc618
hollandro <156948813+hollandro@users.noreply.github.com>
2026-05-12 02:19:54 +0000

    updated files for git pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',  // this is the default, but make it explicit
+  base: '/',
 })
